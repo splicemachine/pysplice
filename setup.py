b039341390555b88e00149fa5da42be0d3bd25c0
Amrit Baveja <amritbaveja@Admins-MacBook-Pro.local>
2018-08-01 10:21:54 -0700

    added apache

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup, find_packages
+# Copyright (c) 2012 - 2017 Splice Machine, Inc.
+#
+# This file is part of Splice Machine.
+# Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+# GNU Affero General Public License as published by the Free Software Foundation, either
+# version 3, or (at your option) any later version.
+# Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License along with
+# Splice Machine. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
 dependencies = [
-"atomicwrites==1.1.5",
-"attrs==18.1.0",
-"more-itertools==4.2.0",
-"pluggy==0.6.0",
-"py==1.5.3",
-"py4j==0.10.7",
-"pyspark==2.3.1",
-"pytest==3.6.1",
-"six==1.11.0"
+    "atomicwrites==1.1.5",
+    "attrs==18.1.0",
+    "more-itertools==4.2.0",
+    "pluggy==0.6.0",
+    "py==1.5.3",
+    "py4j==0.10.7",
+    "pyspark==2.3.1",
+    "pytest==3.6.1",
+    "six==1.11.0"
 ]
 setup(
     name="splicemachine",
--- splicemachine/context.py
+++ splicemachine/context.py
@@ -1,7 +1,22 @@
+"""
+Copyright 2018 Splice Machine, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 from __future__ import print_function
 
-from pyspark.sql import DataFrame
 from py4j.java_gateway import java_import
+from pyspark.sql import DataFrame
 
 
 class PySpliceContext:
@@ -9,25 +24,27 @@ class PySpliceContext:
     This class implements a SpliceMachineContext object (similar to the SparkContext object)
     """
 
-    def __init__(self, JDBC_URL, sparkSQLContext, _unitTesting=False):
+    def __init__(self, JDBC_URL, spark_sql_context, _unit_testing=False):
         """
         :param JDBC_URL: (string) The JDBC URL Connection String for your Splice Machine Cluster
-        :param sparkSQLContext: (sparkContext) A SparkContext Object for executing Spark Queries
+        :param spark_sql_context: (sparkContext) A SparkContext Object for executing Spark Queries
         """
         self.jdbcurl = JDBC_URL
-        self._unitTesting = _unitTesting
+        self._unit_testing = _unit_testing
 
-        if not _unitTesting:  # Private Internal Argument to Override Using JVM
-            self.sparkSQLContext = sparkSQLContext
-            self.jvm = self.sparkSQLContext._sc._jvm
+        if not _unit_testing:  # Private Internal Argument to Override Using JVM
+            self.spark_sql_context = spark_sql_context
+            self.jvm = self.spark_sql_context._sc._jvm
             java_import(self.jvm, "com.splicemachine.spark.splicemachine.*")
-            java_import(self.jvm, "org.apache.spark.sql.execution.datasources.jdbc.{JDBCOptions, JdbcUtils}")
+            java_import(self.jvm,
+                        "org.apache.spark.sql.execution.datasources.jdbc.{JDBCOptions, JdbcUtils}")
             java_import(self.jvm, "scala.collection.JavaConverters._")
-            self.context = self.jvm.com.splicemachine.spark.splicemachine.SplicemachineContext(self.jdbcurl)
+            self.context = self.jvm.com.splicemachine.spark.splicemachine.SplicemachineContext(
+                self.jdbcurl)
 
         else:
             from .utils import FakeJContext
-            self.sparkSQLContext = sparkSQLContext
+            self.spark_sql_context = spark_sql_context
             self.jvm = ''
             self.context = FakeJContext(self.jdbcurl)
 
@@ -37,21 +54,21 @@ class PySpliceContext:
         """
         return self.context.getConnection()
 
-    def tableExists(self, schemaTableName):
+    def tableExists(self, schema_table_name):
         """
         Check whether or not a table exists
 
-        :param schemaTableName: (string) Table Name
+        :param schema_table_name: (string) Table Name
         """
-        return self.context.tableExists(schemaTableName)
+        return self.context.tableExists(schema_table_name)
 
-    def dropTable(self, schemaTableName):  # works
+    def dropTable(self, schema_table_name):  # works
         """
         Drop a specified table.
 
-        :param schemaTableName (optional): (string) schemaName.tableName
+        :param schema_table_name: (optional) (string) schemaName.tableName
         """
-        return self.context.dropTable(schemaTableName)
+        return self.context.dropTable(schema_table_name)
 
     def df(self, sql):
         """
@@ -60,45 +77,45 @@ class PySpliceContext:
         :param sql: (string) SQL Query (eg. SELECT * FROM table1 WHERE column2 > 3)
         :return: A Spark DataFrame containing the results
         """
-        if self._unitTesting:
+        if self._unit_testing:
             return self.context.df(sql)
-        return DataFrame(self.context.df(sql), self.sparkSQLContext)
+        return DataFrame(self.context.df(sql), self.spark_sql_context)
 
-    def insert(self, dataFrame, schemaTableName):
+    def insert(self, dataframe, schema_table_name):
         """
-        Insert a RDD into a table (schema.table).  The schema is required since RDD's do not have schema.
+        Insert a dataframe into a table (schema.table).
 
-        :param dataFrame: (RDD) The dataFrame you would like to insert
-        :param schemaTableName: (string) The table in which you would like to insert the RDD
+        :param dataframe: (DF) The dataframe you would like to insert
+        :param schema_table_name: (string) The table in which you would like to insert the RDD
         """
-        return self.context.insert(dataFrame._jdf, schemaTableName)
+        return self.context.insert(dataframe._jdf, schema_table_name)
 
-    def delete(self, dataFrame, schemaTableName):
+    def delete(self, dataframe, schema_table_name):
         """
         Delete records in a dataframe based on joining by primary keys from the data frame.
         Be careful with column naming and case sensitivity.
 
-        :param dataFrame: (RDD) The dataFrame you would like to delete
-        :param schemaTableName: (string) Splice Machine Table
+        :param dataframe: (DF) The dataframe you would like to delete
+        :param schema_table_name: (string) Splice Machine Table
         """
-        return self.context.delete(dataFrame._jdf, schemaTableName)
+        return self.context.delete(dataframe._jdf, schema_table_name)
 
-    def update(self, dataFrame, schemaTableName):
+    def update(self, dataframe, schema_table_name):
         """
-        Update data from a dataframe for a specified schemaTableName (schema.table).
-        The keys are required for the update and any other columns provided will be updated in the rows.
+        Update data from a dataframe for a specified schema_table_name (schema.table).
+        The keys are required for the update and any other columns provided will be updated
+        in the rows.
 
-        :param dataFrame: (RDD) The dataFrame you would like to update
-        :param schemaTableName: (string) Splice Machine Table
+        :param dataframe: (DF) The dataframe you would like to update
+        :param schema_table_name: (string) Splice Machine Table
         :return:
         """
-        return self.context.update(dataFrame._jdf, schemaTableName)
+        return self.context.update(dataframe._jdf, schema_table_name)
 
-    def getSchema(self, schemaTableName):
+    def getSchema(self, schema_table_name):
         """
         Return the schema via JDBC.
 
-        :param schemaTableName: (string) Table name
+        :param schema_table_name: (DF) Table name
         """
-        return self.context.getSchema(schemaTableName)
-
+        return self.context.getSchema(schema_table_name)
