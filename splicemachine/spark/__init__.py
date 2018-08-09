a81a69fa15f77c8b6506b32ce332e6617237e879
Amrit Baveja <amritbaveja@Admins-MacBook-Pro.local>
2018-08-09 12:50:26 -0700

    BREAKING CHANGES: restructured and added ml zeppelin code

--- splicemachine/spark/__init__.py
+++ splicemachine/spark/__init__.py
@@ -13,23 +13,3 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-from setuptools import setup, find_packages
-
-dependencies = [
-    "atomicwrites==1.1.5",
-    "attrs==18.1.0",
-    "more-itertools==4.2.0",
-    "pluggy==0.6.0",
-    "py==1.5.3",
-    "py4j==0.10.7",
-    "pyspark==2.3.1",
-    "pytest==3.6.1",
-    "six==1.11.0"
-]
-setup(
-    name="splicemachine",
-    version="0.2.2",
-    install_requires=dependencies,
-    packages=find_packages(),
-)
