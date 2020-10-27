b85bf9d61318494b5ceb0e0a919aa069871b7036
Ben Epstein <ben.epstein97@gmail.com>
2020-10-27 13:26:51 -0400

    cleanup init

--- splicemachine/features/__init__.py
+++ splicemachine/features/__init__.py
@@ -1,6 +1,1 @@
-from splicemachine.features.feature_set import *
-from splicemachine.features.feature_store import *
-from splicemachine.features.feature import *
-from splicemachine.features.constants import *
-from splicemachine.features.training_context import *
-from splicemachine.features.utils import *
+from .feature_store import FeatureStore
