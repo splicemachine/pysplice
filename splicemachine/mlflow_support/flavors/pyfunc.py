f6aa94240edce5ab6837706f52ce36e11c8cb4e5
Ben Epstein <ben.epstein97@gmail.com>
2021-01-27 14:50:54 -0700

    trying to support generic log_model function

---
+++ splicemachine/mlflow_support/flavors/pyfunc.py
@@ -0,0 +1,11 @@
+from splicemachine.mlflow_support import *
+from splicemachine.mlflow_support.mlflow_support import _GORILLA_SETTINGS
+import gorilla
+import mlflow.pyfunc
+
+def _log_model(name=None, **flavor_options):
+    if 'python_model' in flavor_options:
+        model = flavor_options.pop('python_model')
+    mlflow.log_model(model, name=name, model_lib='pyfunc', **flavor_options)
+
+gorilla.apply(gorilla.Patch(mlflow.pyfunc, _log_model.__name__.lstrip('_'), _log_model, settings=_GORILLA_SETTINGS))
