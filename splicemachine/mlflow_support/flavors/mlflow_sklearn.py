56403936d7ad0f3b817dc38d8b9be6fd34e21cc4
Ben Epstein <ben.epstein97@gmail.com>
2021-04-12 15:35:04 -0400

    whoops

--- splicemachine/mlflow_support/flavors/mlflow_sklearn.py
+++ splicemachine/mlflow_support/flavors/mlflow_sklearn.py
@@ -1,7 +1,7 @@
 from splicemachine.mlflow_support import *
 from splicemachine.mlflow_support.mlflow_support import _GORILLA_SETTINGS
 import gorilla
-import mlflow.spacy
+import mlflow.sklearn
 
 def _log_model(model, name='sklearn_model', **flavor_options):
     mlflow.log_model(model, name=name, model_lib='sklearn', **flavor_options)
